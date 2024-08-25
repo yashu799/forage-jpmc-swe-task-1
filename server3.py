479f1a590a9e848b5fd006fd07736143cb181a89
yashu799 <yashpatil1204@gmail.com>
2024-08-25 05:40:07 +0000

    Added unit tests for getDataPoint and getRatio and testcases

--- server3.py
+++ server3.py
@@ -216,7 +216,7 @@ def get(req_handler, routes):
                 return
 
 
-def run(routes, host='0.0.0.0', port=8080):
+def run(routes, host='0.0.0.0', port=8081):
     """ Runs a class as a server whose methods have been decorated with
         @route.
     """
@@ -232,7 +232,7 @@ def run(routes, host='0.0.0.0', port=8080):
     thread = threading.Thread(target=server.serve_forever)
     thread.daemon = True
     thread.start()
-    print('HTTP server started on port 8080')
+    print('HTTP server started on port 8081')
     while True:
         from time import sleep
         sleep(1)
